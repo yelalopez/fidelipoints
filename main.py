4c93991e8cd36cd1c9be918de86071c2d592c166
yelalopez <yelitzalopezduque@gmail.com>
2024-11-11 15:41:06 -0300

    feat: publicar e acumular pontos

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import os
+from datetime import datetime
 
 def validar_email(email):
     return "@" in email
@@ -14,16 +15,21 @@ def validar_seguidores(seguidores):
         print("Por favor insira um número válido")
         return False
 
+def obter_path_arquivo(nome_arquivo="usuarios.json"):
+    diretorio_base = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(diretorio_base, nome_arquivo)
+
 def carregar_usuarios(nome_arquivo="usuarios.json"):
+    path_arquivo = obter_path_arquivo(nome_arquivo)
     try:
-        with open(nome_arquivo, "r") as arquivo:
-            usuarios = json.load(arquivo)  
-            return usuarios
+        with open(path_arquivo, "r") as arquivo:
+            return json.load(arquivo)  
     except FileNotFoundError:
         return []  
 
 def salvar_usuarios(usuarios, nome_arquivo="usuarios.json"):
-    with open(nome_arquivo, "w") as arquivo:
+    path_arquivo = obter_path_arquivo(nome_arquivo)
+    with open(path_arquivo, "w") as arquivo:
         json.dump(usuarios, arquivo, indent=4)
 
 def mostrar_dados(usuario):
@@ -143,34 +149,133 @@ def encontrar_usuario(email, senha):
             return usuario
     return None
 
-def menu_usuario(usuario):
+def acumular_pontos(usuario, tipo_post, seguidores, conta_marcada):
+    pontos = 0
+    
+    
+    if tipo_post == 'story':
+        pontos += int(seguidores / 2)
+    elif tipo_post == 'feed':
+        pontos += int(seguidores)
+    
+    if conta_marcada:
+        pontos+=5
+
+    usuario['pontos'] += int(pontos)
+
+    usuarios = carregar_usuarios()
+    for u in usuarios:
+        if u['email'] == usuario['email']:
+            u['pontos'] = usuario['pontos']
+            break
+    salvar_usuarios(usuarios)
+
+    return int(pontos)
+
+def registar_postagem(usuario, tipo_post, conta_marcada):
+    pontos = acumular_pontos(usuario, tipo_post, usuario['seguidores'], conta_marcada)
+
+    nova_postagem = {
+        "conta_marcada" : conta_marcada,
+        "pontos" : pontos,
+        "data" : datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    }
+
+    if 'postagens' not in usuario:
+        usuario['postagens'] = {"story" : [], "feed": []}
+
+    usuario['postagens'][tipo_post].append(nova_postagem)
+
+    usuarios = carregar_usuarios()
+    for u in usuarios:
+        if u['email'] == usuario['email']:
+            u.update(usuario)
+            break
+    salvar_usuarios(usuarios)
+    
     os.system('cls' if os.name == 'nt' else 'clear') or None
-    print(f"\nBem-vindo(a), {usuario['nome']}!")
+    print(f"Postagem registrada! Você ganhou {pontos} pontos.")
+
+def publicar_postagem(usuario):
+    os.system('cls' if os.name == 'nt' else 'clear') or None
+    while True:
+        print('''
+===== Publicar Post =====
+  1. Story
+  2. Feed
+  3. Voltar ao menu
+---------------------------''')
+        try:
+
+            tipo_post = int(input("Escolha uma opção: "))
+
+            if tipo_post == 1:
+                conta_marcada = input('Insira a conta : ')
+                registar_postagem(usuario, 'story', conta_marcada)
+            elif tipo_post == 2:
+                conta_marcada = input('Insira a conta : ')
+                registar_postagem(usuario, 'feed', conta_marcada)
+            elif tipo_post == 3:
+                menu_usuario(usuario)
+                break
+            else:
+                print('Opcão inválida. Tente novamente.')
+        except ValueError:
+            print('Entrada inválida. Por favor intente novamente')
+
+
+def menu_perfil(usuario):
+    os.system('cls' if os.name == 'nt' else 'clear') or None
     while True:
         print('''
 ===== Menu do Usuário =====
   1. Ver perfil
   2. Atualizar perfil
-  3. Meus Fidelipoints
-  4. Deletar perfil           
-  0. Logout
+  3. Deletar perfil           
+  4. Voltar ao menu principal
 ---------------------------''')
-    
         opcao = input("Escolha uma opção: ")
-
         if opcao == "1":
             mostrar_dados(usuario)
         elif opcao == "2":
             atualizar_perfil(usuario)
         elif opcao == "3":
             os.system('cls' if os.name == 'nt' else 'clear') or None
+            deletar_usuario(usuario)
+            break
+        elif opcao == "4":
+            os.system('cls' if os.name == 'nt' else 'clear') or None
+            break
+        else:
+            os.system('cls' if os.name == 'nt' else 'clear') or None
+            print("Opção inválida. Tente novamente.")
+
+        
+def menu_usuario(usuario):
+    os.system('cls' if os.name == 'nt' else 'clear') or None
+    print(f"\nBem-vindo(a), {usuario['nome']}!")
+    while True:
+        print('''
+===== Menu do Usuário =====
+  1. Meu perfil
+  2. Meus Fidelipoints
+  3. Publicar post         
+  0. Logout
+---------------------------''')
+    
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == "1":
+            menu_perfil(usuario)
+        elif opcao == "2":
+            os.system('cls' if os.name == 'nt' else 'clear') or None
             print(f'''
 -----------------------------
 Meus Fidelipoints: {usuario['pontos']}
 -----------------------------''')
-        elif opcao == "4":
+        elif opcao == "3":
             os.system('cls' if os.name == 'nt' else 'clear') or None
-            deletar_usuario(usuario)
+            publicar_postagem(usuario)
             break
         elif opcao == "0":
             os.system('cls' if os.name == 'nt' else 'clear') or None
